7575b3de1d97276d271f1937e841b7a10ccd5172
Hayashi <>
2025-06-29 00:39:52 +0900

    学習履歴管理API作成

---
+++ backend/app/Models/LearningLog.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class LearningLog extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'problem_title',
+        'problem_desc',
+        'status',
+    ];
+
+    public $timestamps = true;
+}
