15e4b15b85574aa424acedf2d58a1d1fa5a37541
Hayashi <>
2025-06-29 19:14:26 +0900

    スキル管理API作成

---
+++ backend/app/Models/Skill.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Skill extends Model
+{
+    protected $fillable = ['name', 'description'];
+    public $timestamps = false;
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,12 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LearningLogController;
+use App\Http\Controllers\SkillController;
+use App\Http\Controllers\UserSkillController;
 
 // Route::middleware('auth:sanctum')->group(function () {
    
 // });
 
+
 Route::post('/learning_logs', [LearningLogController::class, 'store']);
 Route::get('/learning_logs', [LearningLogController::class, 'index']);
 Route::get('/learning_logs/{id}', [LearningLogController::class, 'show']);
 Route::put('/learning_logs/{id}', [LearningLogController::class, 'update']);
+
+Route::get('/skills', [SkillController::class, 'index']);
+Route::post('/user_skills', [UserSkillController::class, 'store']);
+Route::put('/user_skills/{id}', [UserSkillController::class, 'update']);
